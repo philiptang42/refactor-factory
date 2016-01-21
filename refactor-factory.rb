62be128890f370df2975fa07f9f226f23c2bb1e0
philiptang42 <philiptang42@gmail.com>
2016-01-21 18:53:37 -0500

    single line if/unless statements

--- refactor-factory.rb
+++ refactor-factory.rb
@@ -3,13 +3,9 @@ require 'prime'   # This is a module. We'll cover these soon!
 
 def first_n_primes(n)
 
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
+  return "n must be an integer." unless n.is_a? Integer
 
-  if n <= 0
-    return "n must be greater than 0."
-  end
+  return "n must be greater than 0." if n <= 0
 
   prime_array ||= []
 
